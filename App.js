523d24bed036887ec8ea8c3030a30ac0523d10ce
Nahid Hasan <nahidjc80@gmail.com>
2025-03-02 14:34:14 +0600

    animated done

--- App.js
+++ App.js
@@ -7,27 +7,40 @@ import Loader from "./app/components/common/Loader";
 import store from "./app/redux/store";
 import CustomSplashScreen from "./app/screens/SplashScreen";
 
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from "react-native-reanimated";
+
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
 
+  const opacity = useSharedValue(1);
+  const scale = useSharedValue(1);
+
   useEffect(() => {
     const splashTimer = setTimeout(() => {
-      setIsAppReady(true);
-    }, 500);
+      opacity.value = withTiming(0, { duration: 800 });
+      scale.value = withTiming(0.9, { duration: 800 }); 
+
+      setTimeout(() => {
+        setIsAppReady(true);
+      }, 800);
+    }, 2000); 
 
     return () => clearTimeout(splashTimer);
   }, []);
 
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+    transform: [{ scale: scale.value }],
+  }));
+
   return (
     <Provider store={store}>
-      <StatusBar
-        translucent
-        backgroundColor="transparent"
-        barStyle="light-content"
-      />
+      <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
 
       {!isAppReady ? (
-        <CustomSplashScreen />
+        <Animated.View style={[{ flex: 1 }, animatedStyle]}>
+          <CustomSplashScreen />
+        </Animated.View>
       ) : (
         <Suspense fallback={<Loader />}>
           <MainApp />
